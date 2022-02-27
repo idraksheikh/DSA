a604060358aa89f24cd75930e942aead6c9b2d61
idrak sheikh <idraksheikh333@gmail.com>
2022-02-27 16:27:30 +0530

    Question 3

--- prefix&suffix/Question3.java
+++ prefix&suffix/Question3.java
@@ -1,20 +1,56 @@
 import java.util.*;
 import java.io.*;
 public class Question3{
-	public static int[] getProductArrayExceptSelf(int[] arr) {
-    	int[] pro=new int[arr.length];
-    		for(int i=0;i<arr.length;i++){
-        		pro[i]=1;
+	//solution 1
+	// int[] pro=new int[arr.length];
+ //    		for(int i=0;i<arr.length;i++){
+ //        		pro[i]=1;
         	   
-			}
-    	for(int i=0;i<pro.length;i++){
-    		for(int j=0;j<arr.length;j++){
-    			if(i==j){
+	// 		}
+ //    	for(int i=0;i<pro.length;i++){
+ //    		for(int j=0;j<arr.length;j++){
+ //    			if(i==j){
+ //    				continue;
+ //    			}
+ //    			pro[i]=pro[i]*arr[j];
+ //    		}	
+ //    	}
+ //    	return pro;
+	public static int[] getProductArrayExceptSelf(int[] arr) {
+    	//solution 2
+    	int[] pro=new int[arr.length];
+    	int pro1=1;
+    	int pro0=0;
+    	int flag=0;
+    	int count=0;
+    	for(int i=0;i<arr.length;i++){
+    		
+    			if(arr[i]==0) {
+    				flag=1;
+    				count++;
     				continue;
     			}
-    			pro[i]=pro[i]*arr[j];
+    			else{pro1=pro1*arr[i];}
+    		
     		}	
-    	}
+    	for(int i=0;i<pro.length;i++){
+    		
+    			if(flag==1){
+    				if(count>1){
+    					pro[i]=pro0;
+    				}
+    				else{
+	    				if(arr[i]==0){
+	    					pro[i]=pro1;
+	    				}
+	    				else{
+	    					pro[i]=pro0;
+	    				}
+    				}
+    			}
+    			else pro[i]=(pro1/arr[i]);
+    		}
+    	
     	return pro;
         
     }
